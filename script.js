cd2d10890c230910d5f1f5053dc663a9ed50addf
kur1an <amith.kurianj@gmail.com>
2024-06-09 12:11:47 +0530

    ..

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function opentab(event, tabname) {
+    var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName("tab-contents");
+
+    for (var tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (var tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Ensure the first tab is active by default
+    var defaultTab = document.querySelector('.tab-links.active-link');
+    if (defaultTab) {
+        defaultTab.click();
+    } else {
+        document.querySelector('.tab-links').click();
+    }
+});
 document.addEventListener("DOMContentLoaded", function () {
     // Get the buttons
     var pageUpButton = document.getElementById("pageUpButton");
